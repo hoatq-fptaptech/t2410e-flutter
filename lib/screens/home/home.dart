5e5cb14fa2f98dfb952daab050eaf6b31068aeac
quanghoatrinh <quanghoa.trinh@gmail.com>
2026-05-07 20:57:05 +0700

    demo carousel slider

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:t2410e_flutter/screens/home/banner_slider.dart';
 
 class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return const Text("Home page");
+    return const SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: [
+          BannerSlider()
+        ],
+      ),
+    );
   }
 
 }
